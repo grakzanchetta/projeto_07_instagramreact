d1d1a9f5453d286394b12475531e79d86db21e84
Gabriel Rak <grakzanchetta@gmail.com>
2022-05-09 10:11:57 -0300

    Componetização concluida

--- src/Fundo-mobile.js
+++ src/Fundo-mobile.js
@@ -1,6 +1,6 @@
 export default function FundoMobile(){
     return (
-        <div class="fundo-mobile">
+      <div class="fundo-mobile">
         <ion-icon name="home"></ion-icon>
         <ion-icon name="search-outline"></ion-icon>
         <ion-icon name="add-circle-outline"></ion-icon>
